d31f40b41651ee6b33340de7f5093d6875e91d06
QingFeng <66094820+QingFeng-lg@users.noreply.github.com>
2020-06-01 09:00:45 +0800

    Update PoiExcelTest2.java
    优化代码

--- src/main/java/com/qingfeng/test/PoiExcelTest2.java
+++ src/main/java/com/qingfeng/test/PoiExcelTest2.java
@@ -11,10 +11,8 @@ public class PoiExcelTest2 {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        //获取需要解析的文件
-        FileInputStream inputStream = new FileInputStream(new File(SAMPLE_XLSX_FILE_PATH));
         //通过工厂创建工作簿
-        Workbook workbook = WorkbookFactory.create(inputStream);
+        Workbook workbook = WorkbookFactory.create(new FileInputStream(new File(SAMPLE_XLSX_FILE_PATH)));
         //获取第一个表格
         Sheet sheet = workbook.getSheetAt(0);
         DataFormatter dataFormatter = new DataFormatter();
